fbbb08323367eba19c3ebef054389ba35ae653b3
Avi Kasliwal <avi.kasliwal04@gmail.com>
2022-09-18 21:38:07 +0530

    App 2 :  Find The Coin

---
+++ GuessTheCoin/GuessTheCoin/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  GuessTheCoin
+//
+//  Created by Avi Kasliwal on 18/09/22.
+//
+
+import Foundation
---
+++ GuessTheCoin/GuessTheCoin/GuessTheCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheCoinApp.swift
+//  GuessTheCoin
+//
+//  Created by Avi Kasliwal on 18/09/22.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
