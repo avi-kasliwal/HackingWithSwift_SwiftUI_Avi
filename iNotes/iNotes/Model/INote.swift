aa640f10b595be8eae0dea7e0fbfa57163ca56f0
Avi Kasliwal <avi.kasliwal04@gmail.com>
2022-09-28 12:08:32 +0530

    Cleaned up the repo

--- iNotes/iNotes/Model/INote.swift
+++
@@ -1,8 +0,0 @@
-//
-//  INote.swift
-//  iNotes
-//
-//  Created by Avi Kasliwal on 27/09/22.
-//
-
-import Foundation
--- iNotes/iNotes/Model/iNotes.swift
+++
@@ -1,8 +0,0 @@
-//
-//  iNotes.swift
-//  iNotes
-//
-//  Created by Avi Kasliwal on 27/09/22.
-//
-
-import Foundation
